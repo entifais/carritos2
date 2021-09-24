6494322226f6c393823513dd12f086b1c986a087
jero98772 <jero98772@protonmail.com>
2021-09-24 16:12:24 -0500

    update proyect

---
+++ carrito/Sensor.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Sensor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sensor
+{
+    public int tamanio;
+    private String tipo;
+    private double valor;
+}
