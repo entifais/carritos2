edcd5cf16aa2b7be8c14d00e64866be4de9c87cf
jero98772 <jero98772@protonmail.com>
2021-09-25 08:54:11 -0500

    add todo.org, update vehiculo.java

--- carrito/Vehiculo.java
+++ carrito/Vehiculo.java
@@ -6,16 +6,31 @@ public class Vehiculo{
     private double valorComercial;
     private String color;
     Vehiculo[] vehiculos=new Vehiculo[tamano];
-    Vehiculo(){
+    public Vehiculo(){
     }
-    Vehiculo(int mo,String ma,double va){
+    public Vehiculo(int mo,String ma,double va){
         Vehiculo vehiculo=new Vehiculo(mo,ma,va,"color");
     }
-    Vehiculo(int mo,String ma,double va,String co){
+    public Vehiculo(int mo,String ma,double va,String co){
         this.modelo=mo;
         this.marca=ma;
         this.valorComercial=va;
         this.color=co;
+    }
+    public String toString(){
+        String infoVehiculo="modelo:"+this.modelo+"\nmarca:"+this.marca+"\nvalor Comercial:"+String.valueOf(this.valorComercial)+"\ncolor:"+this.color;
+        return infoVehiculo;
+    }
+    public String toStringVeiculos(){
+        String infoVehiculos="";
+        for(int i=0;i<this.tamano;i++){
+            String infoVehiculo="modelo:"+vehiculos[i].modelo+"\nmarca:"+vehiculos[i].marca+"\nvalor Comercial:"+String.valueOf(vehiculos[i].valorComercial)+"\ncolor:"+vehiculos[i].color;
+            infoVehiculos=infoVehiculos+"\tVeiculo"+i+"\n"+infoVehiculo+"\n";
+        }
+        return infoVehiculos;
+    }
+    public int cantidadVehiculos(){
+        return this.posAnadir;
     }
     void setModelo(int mo){this.modelo=mo;}
     void setMarca(String ma){this.marca=ma;}
