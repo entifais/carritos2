2d32c9277c6d3eb03bd2d1f73ebc319baba30654
jero98772 <jero98772@protonmail.com>
2021-09-25 09:55:14 -0500

    working  in sensores.java

--- carrito/Sensor.java
+++ carrito/Sensor.java
@@ -1,13 +1,12 @@
-
-/**
- * Write a description of class Sensor here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Sensor
-{
-    public int tamanio;
+public class Sensor{
+    public int tamano=8;
+    public int posAnadir=0;
     private String tipo;
     private double valor;
+    public Sensor[] sensores=new Sensor[tamano];
+    public Sensor(){}
+    public Sensor(String t,String v){}
+    public String toString(){}
+    public String toStringSensores(){}
+    public int cantidadSensores(){return this.posAnadir;}
 }
--- carrito/Vehiculo.java
+++ carrito/Vehiculo.java
@@ -5,17 +5,20 @@ public class Vehiculo{
     private String marca;
     private double valorComercial;
     private String color;
-    Vehiculo[] vehiculos=new Vehiculo[tamano];
+    public Vehiculo[] vehiculos=new Vehiculo[tamano];
     public Vehiculo(){
+        this.posAnadir=posAnadir+1;
     }
     public Vehiculo(int mo,String ma,double va){
         Vehiculo vehiculo=new Vehiculo(mo,ma,va,"color");
+        this.posAnadir=posAnadir+1;
     }
     public Vehiculo(int mo,String ma,double va,String co){
         this.modelo=mo;
         this.marca=ma;
         this.valorComercial=va;
         this.color=co;
+        this.posAnadir=posAnadir+1;
     }
     public String toString(){
         String infoVehiculo="modelo:"+this.modelo+"\nmarca:"+this.marca+"\nvalor Comercial:"+String.valueOf(this.valorComercial)+"\ncolor:"+this.color;
@@ -29,15 +32,13 @@ public class Vehiculo{
         }
         return infoVehiculos;
     }
-    public int cantidadVehiculos(){
-        return this.posAnadir;
-    }
-    void setModelo(int mo){this.modelo=mo;}
-    void setMarca(String ma){this.marca=ma;}
-    void setValor(double va){this.valorComercial=va;}
-    void setColor(String co){this.color=co;}
-    int getModelo(){return this.modelo;}
-    String getMarca(){return this.marca;}
-    double getValor(){return this.valorComercial;}
-    String getColor(){return this.color;}
+    public int cantidadVehiculos(){return this.posAnadir;}
+    public void setModelo(int mo){this.modelo=mo;}
+    public void setMarca(String ma){this.marca=ma;}
+    public void setValor(double va){this.valorComercial=va;}
+    public void setColor(String co){this.color=co;}
+    public int getModelo(){return this.modelo;}
+    public String getMarca(){return this.marca;}
+    public double getValor(){return this.valorComercial;}
+    public String getColor(){return this.color;}
 }
