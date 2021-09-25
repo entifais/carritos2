a21e59f51bf2bd0df47628549404ab280f24119e
jero98772 <jero98772@protonmail.com>
2021-09-25 17:00:22 -0500

    working to sensor.java and principal.java

--- carrito/Principal.java
+++ carrito/Principal.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 public class Principal{
     public static void main(String[] args){
-	}
-	public void mostrarMenu(){
+    }
+    public void mostrarMenu(){
+        Scanner input = new Scanner(System.in);
+        String banner="                     _ _              ____\n"+"  ___ __ _ _ __ _ __(_) |_ ___   ____//_]|________\n"+" / __/ _` | '__| '__| | __/ _ \\ (o _ |  -|   _  o|\n"+"| (_| (_| | |  | |  | | || (_) |`(_)-------(_)--'\n"+" \\___\\__,_|_|  |_|  |_|\\__\\___/ ";
+        String mensajeMenu="Crear vehiculo[1]\nMostrar informacion del vehiculos[2]\nMostrar canitidad de vehiculos[3]\n Salir[0]";
+        String mensajeDespedida="bye\n\tbye";
+        boolean menuAbierto=true; 
+        String elecion="";
+        while(menuAbierto){
+            System.out.println(banner); 
+            System.out.println(mensajeMenu);
+            elecion="";
+            elecion=input.nextLine();
+            if(elecion.equals("0")||elecion.toLowerCase().equals("salir")){
+                System.out.println(mensajeDespedida);
+                menuAbierto=false;
+                break;
+            }if(elecion.equals("1")||elecion.toLowerCase().equals("crear vehiculo")){
+                //cantidad vehiculos < cantidad en el arreglo if(){}
+            }if(elecion.equals("2")||elecion.toLowerCase().equals("mostrar informacion del vehiculos")||elecion.toLowerCase().equals("mostrar informacion")){}
+        }
     }
 }
--- carrito/Sensor.java
+++ carrito/Sensor.java
@@ -4,9 +4,29 @@ public class Sensor{
     private String tipo;
     private double valor;
     public Sensor[] sensores=new Sensor[tamano];
-    public Sensor(){}
-    public Sensor(String t,String v){}
-    public String toString(){}
-    public String toStringSensores(){}
+    public Sensor(){
+        this.posAnadir=posAnadir+1;
+    }
+    public Sensor(String t,double v){
+        this.tipo=t;
+        this.valor=v;
+        this.posAnadir=posAnadir+1;
+    }
+    public String toStringSensores(){
+        String infoSensores="";
+        for(int i=0;i<this.tamano;i++){
+            String infoSensor="tipo:"+sensores[i].tipo+"\nvalor:"+String.valueOf(sensores[i].valor);
+            infoSensores=infoSensores+"\tSensor"+i+"\n"+infoSensor+"\n";
+        }
+        return infoSensores;
+    }
+    public String toString(){
+        String infoSensor="tipo:"+this.tipo+"\nvalor"+String.valueOf(this.valor);
+        return infoSensor;
+    }
     public int cantidadSensores(){return this.posAnadir;}
+    public void setTipo(String t){this.tipo=tipo;}
+    public void setValor(double v){this.valor=v;}
+    public String getTipo(){return this.tipo;}
+    public double getValor(){return this.valor;}
 }
